020b640fcb0f82e2fa35e19aa6348e4a87971f3e
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2025-06-12 11:42:50 -0700

    Initialize 1.20.1

---
+++ src/main/java/com/example/examplemod/Config.java
@@ -0,0 +1,10 @@
+package com.example.examplemod;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+}
